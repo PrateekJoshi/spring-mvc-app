eebb8b607ccccc4b6c184f32535d1ba803c40336
prateek <prateek@localhost.localdomain>
2018-10-22 20:45:21 +0530

    added docker install script

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ fi
 build_image="spring_mvc_build_image"
 build_container="spring_mvc_build_container"
 
+
 function create_container()
 {
 	docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --force-rm=true -t $build_image .
@@ -30,8 +31,27 @@ function run_app()
 	docker run -p 8181:8181 --name $build_container -v `pwd`:/tmp/build $build_image ./run_app.sh
 }
 
+function install_docker()
+{
+	apt-get update
+	apt-get install \
+     		apt-transport-https \
+     		ca-certificates \
+     		curl \
+     		gnupg2 \
+     		software-properties-common
 
+	curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+	apt-key fingerprint 0EBFCD88
+	add-apt-repository \
+   		"deb [arch=amd64] https://download.docker.com/linux/debian \
+   		$(lsb_release -cs) \
+   		stable"
+	apt-get update
+	apt-get install docker-ce
+}
 
+install_docker
 create_container
 run_container
 delete_container
