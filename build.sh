3afb263c1f56c92f26bce920decf74666bc20d42
root <prateek.joshi@gemalto.com>
2018-10-22 16:04:28 +0530

    added support for proxy in env variable

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+if [ ! -z ${http_proxy} ] 
+then
+	http_proxy="http://"${http_proxy}
+	https_proxy="https://"${https_proxy}
+fi
+
 build_image="spring_mvc_build_image"
 build_container="spring_mvc_build_container"
 
 function create_container()
 {
-	docker build --pull --force-rm=true -t $build_image .
+	docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --pull --force-rm=true -t $build_image .
 }
 
 function run_container()
 {
-	docker run --name $build_container -v `pwd`:/tmp/build $build_image ./maven_build.sh
+	docker run  --name $build_container -v `pwd`:/tmp/build $build_image ./maven_build.sh
 }
 
 
